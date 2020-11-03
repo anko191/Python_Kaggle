633cf8b12c73e83f2248bcf8b8d3290e181bc437
anko191 <anko101022@yahoo.co.jp>
2020-11-03 23:37:19 +0900

    11/03

---
+++ Competition/commits_total.py
@@ -0,0 +1,19 @@
+import numpy as np
+import pandas as pd
+
+# n_seed, n_features,CV_name,n_folds, CV_score, LB_score, 一言コメント
+commits_total = pd.DataFrame(columns = ['n_seed',
+                                'n_features', 'CV_name',
+                                'n_folds', 'CV_score',
+                                'LB_score', 'comment'])
+
+n = 0
+commits_total.loc[n,'n_seed'] = 0
+commits_total.loc[n,'n_features'] = 0
+commits_total.loc[n,'CV_name'] = '?'
+commits_total.loc[n,'n_folds'] = 0
+commits_total.loc[n,'CV_score'] = 0.00
+commits_total.loc[n,'LB_score'] = 0.01
+commits_total.loc[n, 'comment'] = 'nyaaaa'
+
+print(commits_total)
